4a6f90e64b686568460e45bae6ea4b4d83c9a5c4
Mert Celik <mertcelik2399@gmail.com>
2025-04-11 00:36:37 +0300

    basic ops

--- pandas_examples/01_basic_operations.py
+++ pandas_examples/01_basic_operations.py
@@ -1,15 +1,45 @@
 import pandas as pd
-import numpy as np
 
 # Create a DataFrame
 
 # From dictionary
-data = {
+data_dict = {
     'name' : ['Alice', 'Bob', 'Charlie', 'David'],
     'age' : [24, 27, 22, 32],
     'city' : ['New York', 'Los Angeles', 'Chicago', 'Houston'],
 }
 
-df1 = pd.DataFrame(data)
+df1 = pd.DataFrame(data_dict)
 print("\nFrom dictionary:")
 print(df1)
+
+# From list of lists
+data_list = [
+    ['Alice', 24, 'New York'],
+    ['Bob', 27, 'Los Angeles'],
+    ['Charlie', 22, 'Chicago'],
+    ['David', 32, 'Houston'],
+]
+
+df2 = pd.DataFrame(data_list, columns=['name', 'age', 'city'])
+print("\nFrom list:")
+print(df2)
+
+# Basic operations
+print("\nBasic operations:")
+print("Head of DataFrame:\n", df1.head(1))
+
+print("\nDataFrame info:")
+print(df1.info())
+
+print("\nDataFrame description:")
+print(df1.describe())
+
+# Accessing data
+print("\nAccessing data:")
+print("Single column:\n",df1['name'])
+print("\nMultiple columns:\n",df1[['name', 'age']])
+
+# Basic Filtering
+print("\nFiltering:")
+print("Age > 25:\n", df2[df2['age'] > 25])
